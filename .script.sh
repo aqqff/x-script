13fb1a1954ac629980c1b1279006b3dc9d71a882
aqqff <aqqffbaka@gmail.com>
2022-05-26 23:54:47 +0300

    try

---
+++ .script.sh
@@ -0,0 +1,3 @@
+sudo dnf remove gnome-calculator gnome-calendar gnome-clocks gnome-contacts firefox gnome-maps gnome-photos gnome-weather libreoffice-* gnome-tour cheese gnome-text-editor gnome-system-monitor mediawriter gnome-connections totem eog evince simple-scan   
+sudo dnf autoremove
+sudo dnf update
