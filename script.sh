cc0224a18e20745b40ba93fd262b625c3e68ea92
aqqff <aqqffbaka@gmail.com>
2022-05-28 10:03:02 +0300

    fix

--- script.sh
+++ script.sh
@@ -1,3 +1,13 @@
-sudo dnf remove gnome-calculator gnome-calendar gnome-clocks gnome-contacts firefox gnome-maps gnome-photos gnome-weather libreoffice-* gnome-tour cheese gnome-text-editor gnome-system-monitor mediawriter gnome-connections totem eog evince simple-scan   
+sudo dnf remove gnome-calculator gnome-calendar gnome-clocks gnome-contacts firefox gnome-maps gnome-photos gnome-weather libreoffice-* gnome-tour cheese gnome-text-editor gnome-system-monitor mediawriter gnome-connections totem eog evince simple-scan
+sudo dnf remove  colormgr darktable gimp dia entangle gcolor3 hugin scribus synfigstudio xournalpp pitivi 
 sudo dnf autoremove
 sudo dnf update
+sudo dnf upgrade
+sudo dnf install google-chrome-stable
+sudo dnf install snapd
+sudo dnf iwd
+sudo dnf autoremove
+sudo dnf update
+sudo dnf upgrade
+sudo systemctl enable iwd
+sudo systemctl reboot -i
