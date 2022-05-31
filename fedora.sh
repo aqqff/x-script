d8117fea3d9cbd5611cec7d85038571becad475d
aqqff <104072840+aqqff@users.noreply.github.com>
2022-05-31 11:27:18 +0300

    Add files via upload

---
+++ fedora.sh
@@ -0,0 +1,17 @@
+#nmctl device wifi connect "ssid" passowrd "password"
+sudo dnf remove gnome-calculator gnome-calendar gnome-clocks gnome-contacts firefox gnome-maps gnome-photos gnome-weather libreoffice-* gnome-tour cheese gnome-text-editor gnome-system-monitor mediawriter gnome-connections totem eog evince simple-scan gnome-boxes
+sudo dnf remove  colormgr darktable gimp dia entangle gcolor3 hugin scribus synfigstudio xournalpp pitivi
+sudo dnf autoremove
+sudo dnf update
+sudo dnf upgrade
+#sudo dnf install vim
+#sudo dnf install snapd
+#sudo dnf install google-chrome-stable
+#sudo fedora-third-party enable
+#hostnamectl hostname "name" 
+#sudo cp -r "/copy" "/past"
+#touch ~/.vimrc
+sudo dnf autoremove
+sudo dnf update
+sudo dnf upgrade
+
