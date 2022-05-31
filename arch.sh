eb490eec529ac9ec9229eff064ab5950ca4adf2a
aqqff <aqqffbaka@gmail.com>
2022-05-31 19:12:17 +0300

    fix

--- arch.sh
+++ arch.sh
@@ -1,13 +1,31 @@
-sudo pacman -R gnome-calculator gnome-calendar gnome-clocks gnome-contacts firefox gnome-maps gnome-photos gnome-weather gnome-tour cheese gedit gnome-system-monitor gnome-connections totem eog evince simple-scan gnome-boxes
+#nmcli device wifi connect "ssid" passowrd "passowrd"
+
+sudo pacman -R gnome-calculator gnome-calendar gnome-clocks gnome-contacts gnome-maps gnome-photos gnome-weather cheese gedit gnome-system-monitor totem eog evince simple-scan gnome-boxes gnome-books sushi
 sudo pacman -R $(pacman -Qdtq)
 sudo pacman -Syu
+
 #sudo pacman -S fish
 #sudo pacman -S git
 #cd ~/Downloads
+
+#pamac-aur
+
+#git clone https://aur.archlinux.org/pamac-aur.git
+#cd pamac-aur/
+#makepkg -si
+
+#yay
+
 #git clone https://aur.archlinux.org/yay.git
 #cd yay/
 #makepkg -si
+
+#vim
+
 #sudo pacman -S vim
+
+#snap
+
 #cd ~/Downloads
 #git clone https://aur.archlinux.org/snapd.git
 #cd snapd
@@ -15,21 +33,31 @@ sudo pacman -Syu
 #sudo systemctl enable --now snapd.socket
 #sudo ln -s /var/lib/snapd/snap /snap
 #sudo snap isntall snap-store
+
+#flatpak
+
 #sudo pacman -S flatpak
-#wget https://go.microsoft.com/fwlink?linkid=2149051
-#ar x ~/Downloads/
-#cd ~/Downloads
+
+#sudo pacman -Syu
+#sudo pacman -R $(pacman -Qdtq)
+
 #microsoft edge install
+#cd ~/Downloads
 #wget https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_101.0.1210.53-1_amd64.deb
+
 #alt.
 #yay -S debtap
 #cd ~/Downloads
-#debtap microsoft-edge-stable_101.0.1210.53-1_amd64.deb
+#sudo debtap -u microsoft-edge-stable_101.0.1210.53-1_amd64.deb
 #sudo pacman -U microsoft-edge-stable_101.0.1210.53-1_amd64.zst
-#or alt.
+
+#Manually
+#ar x ~/Downloads
 #tar xvf control.tar.gz
 #tar xvf data.tar.gz
 #sudo mv ./etc/* /etc/
 #sudo mv ./opt/* /opt/
 #sudo mv ./usr/* /usr/
+#sudo pacman -Syu
+#sudo pacman -R $(pacman -Qtdq)
 
