48a0286a9cf7740f7542fb2b6c42f45262e10a4c
aqqff <aqqffbaka@gmail.com>
2022-05-31 17:57:36 +0300

    arch

---
+++ arch.sh
@@ -0,0 +1,35 @@
+sudo pacman -R gnome-calculator gnome-calendar gnome-clocks gnome-contacts firefox gnome-maps gnome-photos gnome-weather gnome-tour cheese gedit gnome-system-monitor gnome-connections totem eog evince simple-scan gnome-boxes
+sudo pacman -R $(pacman -Qdtq)
+sudo pacman -Syu
+#sudo pacman -S fish
+#sudo pacman -S git
+#cd ~/Downloads
+#git clone https://aur.archlinux.org/yay.git
+#cd yay/
+#makepkg -si
+#sudo pacman -S vim
+#cd ~/Downloads
+#git clone https://aur.archlinux.org/snapd.git
+#cd snapd
+#makepkg -si
+#sudo systemctl enable --now snapd.socket
+#sudo ln -s /var/lib/snapd/snap /snap
+#sudo snap isntall snap-store
+#sudo pacman -S flatpak
+#wget https://go.microsoft.com/fwlink?linkid=2149051
+#ar x ~/Downloads/
+#cd ~/Downloads
+#microsoft edge install
+#wget https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_101.0.1210.53-1_amd64.deb
+#alt.
+#yay -S debtap
+#cd ~/Downloads
+#debtap microsoft-edge-stable_101.0.1210.53-1_amd64.deb
+#sudo pacman -U microsoft-edge-stable_101.0.1210.53-1_amd64.zst
+#or alt.
+#tar xvf control.tar.gz
+#tar xvf data.tar.gz
+#sudo mv ./etc/* /etc/
+#sudo mv ./opt/* /opt/
+#sudo mv ./usr/* /usr/
+
