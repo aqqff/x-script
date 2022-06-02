63d96293cd8e2c10e5fed2800c02518d665486ab
aqqff <aqqffbaka@gmail.com>
2022-06-02 09:48:54 +0000

    add pop

---
+++ pop.sh
@@ -0,0 +1,10 @@
+sudo apt remove gnome-calculator gnome-calendar gnome-contacts geary gnome-text-editor gnome-weather libreoffice* firefox gnome-system-monitor gnome-connections totem eog evince simple-scan 
+sudo apt update
+sudo apt upgrade
+#sudo apt install vim
+#sudo apt install snapd
+#hostnamectl hostname "name"
+sudo apt update
+sudo apt upgrade
+sudo apt autoremove
+
